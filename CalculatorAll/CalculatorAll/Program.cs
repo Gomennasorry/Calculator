6ff1ae10223dc66169e5d61062f0b4cde0b44f98
Gomennasorry <79724773+Gomennasorry@users.noreply.github.com>
2024-05-10 12:12:41 +0700

    01

---
+++ CalculatorAll/CalculatorAll/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddControllersWithViews();
+if (builder.Environment.IsDevelopment())
+{
+    // comment this if u run IIS
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+}
+else
+{
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRazorPages();
+
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
